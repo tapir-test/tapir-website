198a0c554e85c449c69c70366b09d5ee068e740d
Oliver Libutzki <oliver.libutzki@bmiag.de>
2018-01-12 14:16:44 +0100

    Clean up
    Signed-off-by: Oliver Libutzki <oliver.libutzki@bmiag.de>

--- js/formspree.js
+++ js/formspree.js
@@ -1,11 +1,11 @@
 $(function() {
 	var $contactForm = $('#contact-form');
-    var $contactFormSubmissionResult = $('#contact-form-submission-result');
+
 	$contactForm.on('submit', function(e) {
 		var msgLoad = '<div class="alert alert-dismissible alert-info"><button type="button" class="close" data-dismiss="alert">&times;</button><h4 class="alert-heading">Sending message…</h4>Please wait a second...</div>';
 		var msgSuccess = '<div class="alert alert-dismissible alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button><h4 class="alert-heading">Message sent!</h4>Thanks for contacting us. We will answer your query promptly.</div>';
 		var msgError = '<div class="alert alert-dismissible alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button><h4 class="alert-heading">Message NOT sent!</h4>Something went wrong. Please try to contact us via <a class="alert-link" href="mailto:tapir@bmiag.de?subject=tapir">tapir@bmiag.de</a></div>';
-
+        var $contactFormSubmissionResult = $('#contact-form-submission-result');
 		e.preventDefault();
 		$.ajax({
 			url: '//formspree.io/tapir@bmiag.de',
--- js/main.js
+++ js/main.js
@@ -6,6 +6,4 @@ $(function() {
     // Markdown plain out to bootstrap style
     $('#markdown-content-container table').addClass('table');
     $('#markdown-content-container img').addClass('img-responsive');
-
-
 });
